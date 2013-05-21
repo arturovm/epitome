8581cc1cdfc0e81065f9c58a8874f8c14e09835f
Arturo Vergara <songoftheseloth@gmail.com>
2013-05-20 22:33:14 -0500

    Added a static file server

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	m := RegisterRoutes()
 	http.Handle("/api/",  m)
-	http.Handle("/", http.FileServer(http.Dir("")))
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("static"))))
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
