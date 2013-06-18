ad5a51a52ddf830c83ade1cfcd80210faadfe464
Arturo Vergara <songoftheseloth@gmail.com>
2013-06-18 13:25:19 -0500

    Delete subscriptions, get subscriptions first draft. Updated routes

--- routes.go
+++ routes.go
@@ -13,16 +13,11 @@ func RegisterRoutes() *pat.PatternServeMux {
 	// Subscriptions
 	m.Post("/api/subscriptions", http.HandlerFunc(PostSubscription))
 	m.Get("/api/subscriptions", http.HandlerFunc(GetSubscriptions))
-	/*
-	* Regarding GET /subscriptions:
-	* Should this list the user's subscriptions? Or return all his articles (with proper filters and such)?
-	* The RESTful thing to do seems to be the first option. But then, what endpoint should return all of the user's articles?
-	 */
 	m.Del("/api/subscriptions/:id", http.HandlerFunc(DeleteSubscription))
 	// Articles
 	m.Get("/api/subscriptions/articles", http.HandlerFunc(GetAllArticles))
-	m.Get("/api/subscriptions/:id", http.HandlerFunc(GetArticles))
-	m.Put("/api/subscriptions/:subid/:artid", http.HandlerFunc(PutArticle))
+	m.Get("/api/subscriptions/:id/articles", http.HandlerFunc(GetArticles))
+	m.Put("/api/subscriptions/:subid/articles/:artid", http.HandlerFunc(PutArticle))
 	// Favorites
 	m.Post("/api/favorites", http.HandlerFunc(PostFavorites))
 	m.Get("/api/favorites", http.HandlerFunc(GetFavorites))
--- subscriptions.go
+++ subscriptions.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"errors"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/moovweb/gokogiri"
@@ -12,9 +13,9 @@ import (
 )
 
 type Subscription struct {
-	Id   string
-	Url  string
-	Name string
+	Id   int    `json:"id"`
+	Url  string `json:"url"`
+	Name string `json:"name"`
 }
 
 func removeTrail(rawurl string) string {
@@ -83,28 +84,76 @@ func findRSSTitle(rssUrl string) (string, error) {
 }
 
 func PostSubscription(w http.ResponseWriter, req *http.Request) {
-	// Auth should go here, but well...
+	//TODO:  Auth should go here, but well...
 	if rawurl := req.FormValue("url"); rawurl != "" {
 		rssUrl, err := findRSSURL(rawurl)
 		if err != nil {
-			// TODO: return error here
-			log.Fatal(err)
+			// TODO: Write error func. How should the program decide what status code to send?
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			log.Printf("POST Subscription error (Find URL error): %s", err.Error())
 		} else {
-			log.Print(rssUrl)
 			title, err := findRSSTitle(rssUrl)
 			if err != nil {
-				// TODO: return error here
-				log.Fatal(err)
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
+				log.Printf("POST Subscription error (Find title error): %s", err.Error())
 			}
 			DB, _ := sql.Open("sqlite3", "db.db")
 			DB.Exec("insert into subscriptions values (null, ?, ?)", rssUrl, title)
 			DB.Close()
+			w.WriteHeader(http.StatusCreated)
+			w.Write([]byte(""))
 		}
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Insufficient parameters: URL was not provided"))
 	}
 }
 
 func GetSubscriptions(w http.ResponseWriter, req *http.Request) {
+	// TODO: Auth
+	DB, _ := sql.Open("sqlite3", "db.db")
+	rows, err := DB.Query("select * from subscriptions")
+	DB.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		log.Printf("GET Subscriptions error (Database error): %s", err.Error())
+	}
+	subs := make([]Subscription, 0)
+	for rows.Next() {
+		var sub Subscription
+		rows.Scan(&sub.Id, &sub.Url, &sub.Name)
+		subs = append(subs, sub)
+	}
+	rows.Close()
+	enc := json.NewEncoder(w)
+	w.Header().Set("content-type", "application/json")
+	encErr := enc.Encode(subs)
+	if encErr != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(""))
+		log.Printf("GET Subscriptions error (JSON encoding error): %s", err.Error())
+	}
 }
 
 func DeleteSubscription(w http.ResponseWriter, req *http.Request) {
+	// TODO: Auth
+	id := req.URL.Query().Get(":id")
+	var sub Subscription
+	DB, _ := sql.Open("sqlite3", "db.db")
+	defer DB.Close()
+	err := DB.QueryRow("select * from subscriptions where id=?", id).Scan(&sub.Id, &sub.Url, &sub.Name)
+	switch {
+	case err == sql.ErrNoRows:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Subscription does not exist"))
+	case err != nil:
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		log.Printf("DELETE subscription error (Database error): %s", err.Error())
+	default:
+		DB.Exec("delete from subscriptions where id=?", id)
+	}
 }
