db11ef18cba0399c899da3a35be6a8a3e30e7dd0
Arturo Vergara <songoftheseloth@gmail.com>
2013-10-24 19:05:33 -0500

    Tables now support global subs and user subs

--- main.go
+++ main.go
@@ -23,9 +23,10 @@ func createTables() {
 	DB, _ := sql.Open("sqlite3", ExePath+"/db.db")
 	DB.Exec("create table users (id integer primary key, username text, display_name text, password_hash text, role integer)")
 	DB.Exec("create table sessions (id integer primary key, username text, session_token text, app_name text, created_at string)")
-	DB.Exec("create table subscriptions (id integer primary key, user_id integer references users(id), url text, name text)")
+	DB.Exec("create table subscriptions (id integer primary key, url text, name text)")
+	DB.Exec("create table user_subscriptions (id integer primary key, subscription_id integer references subscriptions(id), user_id integer references users(id))")
 	DB.Exec("create table articles (id integer primary key, subscription_id integer references subscriptions(id), url text, name text, author text, published integer, body text, read bool)")
-	DB.Exec("create table read_articles (id integer primary key, user_id integer references users(id), article_id integer references articles(id), read bool)")
+	DB.Exec("create table user_read_articles (id integer primary key, user_id integer references users(id), article_id integer references articles(id), read bool)")
 	DB.Exec("create table favorites (id integer primary key, url text, name text, author text, published integer, body text)")
 	// select articles.id, url, author, published, subscription_id, body, read_articles.read from articles inner join read_articles on articles.id = read_articles.article_id <-- read articles
 	// select articles.id, url, author, published, subscription_id, body, articles.read from articles left outer join read_articles on articles.id = read_articles.article_id where read_articles.id is null <-- unread articles
