d7add2c56125e9a7ca8bb372d0749c9f7bc62ed5
Arturo Vergara <songoftheseloth@gmail.com>
2013-11-13 18:13:06 -0600

    Permissions to create new users now work as they should.

--- users.go
+++ users.go
@@ -26,49 +26,46 @@ type User struct {
 }
 
 func PostUser(w http.ResponseWriter, req *http.Request) {
-	//TODO: check current user here and user role. For now, only check if open to public
-	if UserPreferences.NewUserPermissions == PublicRole {
-		username := req.FormValue("username")
-		usernameLower := strings.ToLower(username)
-		password := req.FormValue("password")
-		var role UserRole
-		switch req.FormValue("role") {
-		case "admin":
-			role = AdminRole
-		case "normal":
-			role = NormalRole
-		}
-		if username == "" || password == "" {
-			WriteJSONError(w, http.StatusBadRequest, "Insufficient parameters")
-			return
-		}
-		hash, err := bcrypt.GenerateFromPassword([]byte(password), 12)
-		if err != nil {
-			WriteJSONError(w, http.StatusInternalServerError, "Bcrypt Error")
-			return
-		}
-		DB, err := sql.Open("sqlite3", ExePath+"/db.db")
-		if err != nil {
-			WriteJSONError(w, http.StatusInternalServerError, "Couldn't connect to database")
-			return
-		}
-		defer DB.Close()
+	uAuth, err, _ := GetUserForSessionToken(req.Header.Get("x-session-token"))
+	if UserPreferences.NewUserPermissions != PublicRole && (uAuth == nil || uAuth.Role > UserPreferences.NewUserPermissions) {
+		WriteJSONError(w, http.StatusUnauthorized, "Insufficient permissions to create new accounts")
+		return
+	}
+	var role UserRole
+	role = NormalRole
+	if req.FormValue("role") == "admin" {
+		role = AdminRole
+	}
+	username := req.FormValue("username")
+	password := req.FormValue("password")
+	if username == "" || password == "" {
+		WriteJSONError(w, http.StatusBadRequest, "Insufficient parameters")
+		return
+	}
+	usernameLower := strings.ToLower(username)
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		WriteJSONError(w, http.StatusInternalServerError, "Bcrypt Error")
+		return
+	}
+	DB, err := sql.Open("sqlite3", ExePath+"/db.db")
+	if err != nil {
+		WriteJSONError(w, http.StatusInternalServerError, "Couldn't connect to database")
+		return
+	}
+	defer DB.Close()
 
-		var u User
-		err = DB.QueryRow("select * from users where username = ?", usernameLower).Scan(&u.Id, &u.Username, &u.DisplayName, &u.PasswordHash, &u.Role)
-		if err == sql.ErrNoRows {
-			_, err = DB.Exec("insert into users values (null, ?, ?, ?, ?)", usernameLower, username, hash, role)
-			if err != nil {
-				WriteJSONError(w, http.StatusInternalServerError, "Unable to write to database")
-				return
-			}
-			w.WriteHeader(http.StatusCreated)
-		} else {
-			WriteJSONError(w, http.StatusConflict, "User already exists")
+	var u User
+	err = DB.QueryRow("select * from users where username = ?", usernameLower).Scan(&u.Id, &u.Username, &u.DisplayName, &u.PasswordHash, &u.Role)
+	if err == sql.ErrNoRows {
+		_, err = DB.Exec("insert into users values (null, ?, ?, ?, ?)", usernameLower, username, hash, role)
+		if err != nil {
+			WriteJSONError(w, http.StatusInternalServerError, "Unable to write to database")
 			return
 		}
+		w.WriteHeader(http.StatusCreated)
 	} else {
-		WriteJSONError(w, http.StatusUnauthorized, "You have insufficient permissions to create new accounts")
+		WriteJSONError(w, http.StatusConflict, "User already exists")
 		return
 	}
 }
