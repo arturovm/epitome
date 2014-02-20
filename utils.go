d1aab2deb122ac03b6b0bdfe20462bceae224230
Arturo Vergara <songoftheseloth@gmail.com>
2014-02-20 09:44:12 -0600

    Updated utils.go from develop

--- utils.go
+++ utils.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"net/http"
+	"strings"
 )
 
 func WriteJSONError(w http.ResponseWriter, status int, errorMessage string) {
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(status)
 	w.Write([]byte(`{"error": "` + errorMessage + `"}`))
+}
+
+func TestContentType(rawHeader *string, target string) bool {
+	parts := strings.Split(*rawHeader, ";")
+	if len(parts) > 0 {
+		return strings.Trim(parts[0], " \t") == target
+	}
+	return false
 }
